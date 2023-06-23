a04836b394abdd8901a1ada6853b73b84be247b1
Alexander Lindenbaum <lindenbaum.alexander@gmail.com>
2023-06-23 18:48:52 -0400

    Implemented zk proving and verification. Test in web/script.js. pot ceremony done once

--- web/modules/zk.js
+++ web/modules/zk.js
@@ -1,36 +1,46 @@
-// Code mostly due to snark.js documentation: https://github.com/iden3/snarkjs
+const crypto = window.crypto;
+const zk_protocol = snarkjs.groth16;
 
-// async function calculateProof() {
-
-//     const { proof, publicSignals } =
-//       await snarkjs.groth16.fullProve( { a: 3, b: 11}, "circuit.wasm", "circuit_final.zkey");
-
-//     proofComponent.innerHTML = JSON.stringify(proof, null, 1);
-
-
-//     const vkey = await fetch("verification_key.json").then( function(res) {
-//         return res.json();
-//     });
-
-//     const res = await snarkjs.groth16.verify(vkey, publicSignals, proof);
-
-//     resultComponent.innerHTML = res;
-// }
-
-const prover = snarkjs.groth16;
-
-async function proveValidBoard() {
-
+function generateSalt() {
+    // Really, k should be a random field element. But a 32-bit salt is sufficient.
+    return crypto.getRandomValues(new Uint32Array(1))[0];
 }
-
-async function verifyValidBoard() {
-
-}
-
-async function proveHitShip() {
-
-}
-
-async function verifyHitShip() {
+async function proveValidBoard(board) {
+    const salt = generateSalt();
+    const input = { board, salt };
     
+    const { proof, publicSignals } = await zk_protocol.fullProve(input, 'assets/boardID.wasm', 'assets/boardID.zkey');
+
+    return { salt, boardID: publicSignals[0], boardIDProof: proof };
 }
+
+async function verifyValidBoard(boardID, proof) {
+    const vkey = await fetch("assets/boardID_verification.json").then( function(res) {
+        return res.json();
+    });
+
+    const res = await zk_protocol.verify(vkey, [boardID], proof); // True or false
+    return res
+}
+
+
+
+
+
+async function proveHitShip(board, salt, boardID, attack_i, attack_j) {
+    const input = { board, salt, boardID, attack_i, attack_j };
+
+    const { proof, publicSignals } = await zk_protocol.fullProve(input, 'assets/hitShip.wasm', 'assets/hitShip.zkey');
+    return { hitShip: publicSignals[0], hitShipProof: proof } // publicSignals[0] is '1'==true or '0'==false
+}
+
+async function verifyHitShip(boardID, attack_i, attack_j, hitShip, proof) {
+    const vkey = await fetch("assets/hitShip_verification.json").then( function(res) {
+        return res.json();
+    });
+
+    const res = await zk_protocol.verify(vkey, [hitShip, boardID, attack_i, attack_j], proof); // True or false
+    return res
+}
+
+export { proveValidBoard, proveHitShip, verifyValidBoard, verifyHitShip };
--- web/script.js
+++ web/script.js
@@ -1,3 +1,20 @@
 import { contract } from './modules/contractInterface.js';
+import { proveValidBoard, proveHitShip, verifyValidBoard, verifyHitShip } from './modules/zk.js';
 
-console.log(contract.functions)
+const board = [[1,0,0], [1,0,0], [0, 0, 1]];
+const { salt, boardID, boardIDProof } = await proveValidBoard(board);
+console.log('FIRST PROOF');
+console.log(salt, boardID, boardIDProof);
+
+console.log('VERIFIED?');
+const res = await verifyValidBoard(boardID, boardIDProof);
+console.log(res);
+
+const attack_i = 2; const attack_j = 2; // Hit
+const { hitShip, hitShipProof } = await proveHitShip(board, salt, boardID, attack_i, attack_j);
+console.log('SECOND PROOF');
+console.log(hitShip, hitShipProof);
+
+console.log('SECOND VERIFICATION?');
+const res2 = await verifyHitShip(boardID, attack_i, attack_j, hitShip, hitShipProof);
+console.log(res2);
